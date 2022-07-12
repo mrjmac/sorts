902f8607029ba7cf6209abc1c0ef2d36f4737634
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-07-12 15:25:43 -0500

    sleepsort and readme changes

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ A lossy sort whose concept was shamelessly stolen from https://www.dangermouse.n
 Drops all numbers that aren't currently in order
 
 While you could use this for important things I would not recommend doing so
+
+# sleepsort
+Concept stolen from somewhere, I don't recall where though
+
+Creates a thread for each number which sleeps for the number
+
+Any issues with this are due to the time it takes to create a thread, to fix this change the static divisors. 
---
+++ sleepsort.py
@@ -0,0 +1,24 @@
+from concurrent.futures import thread
+from threading import Thread
+from time import sleep
+
+# sleep function
+def wait(i) :
+    # for negative numbers, divide a constant by them in order to make larger(which are in turn lower) numbers print first
+    if i < 0 :
+        sleep(.5 / abs(i))
+    # for positive numbers, divide by a constant to decrease time spent waiting
+    else :
+        sleep(i / 5)
+    # print the number
+    print(i)
+
+# take in a string of numbers and cast them all to ints before adding them to a list
+nums = list(map(int, input("Enter nums in format x x x x (ex: 1 2 3 4 5) :: ").split()))
+
+# iterate through the list and call our sleep function on every number in an individual thread
+for item in nums :
+    x = Thread(target=wait, args=(item,)).start()
+    
+    
+
