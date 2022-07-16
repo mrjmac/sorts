890524eaabe80107fc3dba5c6f7cbc3463cdc0fe
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-07-16 03:10:25 -0500

    bubble and insertion sort added

---
+++ bubblesort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class bubblesort {
+    public static void main(String[] args)
+    {
+        
+        //Read in numbers
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter nums in format x x x x (ex: 1 2 3 4 5) :: ");
+        String nums[] = read.nextLine().split(" ");
+        System.out.println();
+
+        //convert numbers into an integer arraylist
+        ArrayList<Integer> sort = new ArrayList<Integer>();
+        for (String a : nums)
+        {
+            sort.add(Integer.parseInt(a));
+        }
+        System.out.println(sort.toString());
+        
+        //perform sort
+        for (int i = 0; i < sort.size() - 1; i++)
+        {
+            for (int n = 0; n < sort.size() - 1 - i; n++)
+            {
+                if (sort.get(n) > sort.get(n+1))
+                {
+                    //swap nums
+                    sort = swap(sort, n);
+                }
+            }
+        }
+
+        System.out.println(sort);
+        read.close();
+    
+    }
+
+    //simple method to swap two items in an array
+    public static ArrayList<Integer> swap(ArrayList<Integer> list, int spot)
+    {
+        list.set(spot, list.set(spot + 1, list.get(spot)));
+        return list;
+    }
+}
---
+++ insertionsort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class insertionsort {
+    public static void main(String[] args)
+    {
+        
+        //Read in numbers
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter nums in format x x x x (ex: 1 2 3 4 5) :: ");
+        String nums[] = read.nextLine().split(" ");
+        System.out.println();
+
+        //convert numbers into an integer arraylist
+        ArrayList<Integer> sort = new ArrayList<Integer>();
+        for (String a : nums)
+        {
+            sort.add(Integer.parseInt(a));
+        }
+        System.out.println(sort.toString());
+        
+        //perform sort
+        for (int i = 1; i < sort.size(); i++)
+        {
+            int curr = sort.get(i);
+            int idx = i - 1;
+            while (idx >= 0 && sort.get(idx) > curr)
+            {
+                sort.set(idx + 1, sort.get(idx));
+                idx -= 1;
+            }
+            sort.set(idx + 1, curr);
+        }
+
+        System.out.println(sort);
+        read.close();
+    
+    }
+
+}
